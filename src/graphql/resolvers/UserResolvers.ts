5681ed9d8f23d77bad78d7653c866b8563029796
Vestertech <vesteerwood@gmail.com>
2024-04-14 05:38:08 +0100

    mutations and Resolve fields

--- src/graphql/resolvers/UserResolvers.ts
+++ src/graphql/resolvers/UserResolvers.ts
@@ -1,8 +1,18 @@
-import { Args, Int, Query, Resolver } from '@nestjs/graphql';
+import {
+  Args,
+  Int,
+  Mutation,
+  Parent,
+  Query,
+  ResolveField,
+  Resolver,
+} from '@nestjs/graphql';
 import { User } from '../models/User';
 import { mockUsers } from 'src/__mocks__/mockUsers';
-
-@Resolver()
+import { UserSetting } from '../models/userSettings';
+import { mockUserSettings } from 'src/__mocks__/`mockUserSettings';
+export let incrementalId = 3;
+@Resolver(() => User)
 export class UserResolver {
   @Query((returns) => User, { nullable: true })
   getUserById(@Args('id', { type: () => Int }) id: number) {
@@ -12,5 +22,21 @@ export class UserResolver {
   @Query(() => [User])
   getUsers() {
     return mockUsers;
+  }
+  // Used to retrieve nested data
+  @ResolveField((returns) => UserSetting, { name: 'settings', nullable: true })
+  getUserSettings(@Parent() user: User) {
+    // console.log(user);
+    return mockUserSettings.find((setting) => setting.userId === user.id);
+  }
+
+  @Mutation((returns) => User)
+  createUser(
+    @Args('username') username: string,
+    @Args('displayName', { nullable: true }) displayName: string,
+  ) {
+    const newUser = { username, displayName, id: ++incrementalId };
+    mockUsers.push(newUser);
+    return newUser;
   }
 }
